6f6e19c1c6f77568f8a9c17b4dd9fc80a378f276
John F <15612814+usernotnull@users.noreply.github.com>
2025-12-20 10:00:03 +0400

    Update 02-config.sh

--- modules-system/02-config.sh
+++ modules-system/02-config.sh
@@ -1,44 +1,24 @@
 #!/usr/bin/env bash
 # Module to configure system services and apply fixes.
 
-echo "Starting system configuration..."
+echo "Starting system configuration…"
 
 # Enable TLP (Power Management) Service
-# echo "Enabling TLP service to start on boot..."
+# echo "Enabling TLP service to start on boot…"
 # systemctl enable tlp
 
-# Remove conflicting audio package (fixes issue with playing audio)
-# echo "Removing gstreamer1.0-vaapi (Audio fix)..."
-# apt remove -y gstreamer1.0-vaapi
-
 # --- Firewall Configuration (System Protection) ---
-echo "Configuring UFW firewall rules for Syncthing access (System Protection)..."
+echo "Configuring UFW firewall rules for Syncthing access (System Protection)…"
 # Allow Syncthing's default TCP port for synchronization
-echo "Allowing TCP port 22000 (Syncthing Synchronization)..."
+echo "Allowing TCP port 22000 (Syncthing Synchronization)…"
 ufw allow 22000/tcp
 # Allow Syncthing's default UDP port for discovery
-echo "Allowing UDP port 21027 (Syncthing Discovery)..."
+echo "Allowing UDP port 21027 (Syncthing Discovery)…"
 ufw allow 21027/udp
 
-echo "Setting default file associations… [skipped]"
+echo "Setting default file associations…"
 
-# xdg-mime default vlc.desktop \
-#     video/mp4 \
-#     video/x-matroska \
-#     video/webm \
-#     video/quicktime \
-#     video/avi \
-#     video/mpeg \
-#     video/x-ms-wmv \
-#     video/x-ms-asf \
-#     video/3gpp \
-#     video/3gpp2 \
-#     video/mp2t \
-#     audio/mpeg \
-#     audio/flac \
-#     audio/wav \
-#     audio/ogg \
-#     audio/mp4 \
-#     audio/midi
+grep -E '^(audio/|video/)' /usr/share/mime/types | xargs xdg-mime default vlc.desktop
+grep -E '^(text/|application/(javascript|json|xml|x-shellscript|x-yaml|x-python|x-php|x-perl|x-ruby))' /usr/share/mime/types | xargs xdg-mime default code.desktop
 
 echo "System configuration complete."
